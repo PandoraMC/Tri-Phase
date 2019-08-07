cbcf134f5349b556ef49f696e75f8100d9fd7d58
PandoraMC <miguel.castillo.pandora@outlook.com>
2019-08-07 13:50:42 -0500

    6 phase adjustment

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,13 +1,13 @@
-int sig[] = {5, 1, 3, 2, 6, 4};
+int sig[] = {0x31, 0x23, 0x07, 0x0E, 0x1C, 0x38};
 int i = 0;
 
 void setup() {
   
-  DDRB = 0x07;
+  DDRB = 0x3F;
   
   TCCR1A = 0x01;
   TCCR1B = 0x15;
-  OCR1AL = 0x50;
+  OCR1AL = 0x28;
   TIMSK1 = 0x02;
   
   sei();
